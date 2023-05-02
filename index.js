588c8a66b89da51745f03eae9a16d46070d90b48
Isaac-ngahu <isaac.ngahu@student.moringaschool.com>
2023-05-02 20:32:31 +0300

    partially done

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Your code here
+function createEmployeeRecord(array){
+    let obj={}
+    obj.firstName=array[0]
+    obj.familyName=array[1]
+    obj.title=array[2]
+    obj.payPerHour=array[3]
+    obj.timeInEvents=[]
+    obj.timeOutEvents=[]
+return obj;
+}
+ 
+function createEmployeeRecords(twoRows){
+    let employeeRecords=[]
+    for(let i=0;i<twoRows.length;i++){
+   const Array=twoRows[i]
+   employeeRecords.push(createEmployeeRecord(Array))
+    }
+   
+   return employeeRecords
+}
+function createTimeInEvent(object,dateStamp="YYYY-MM-DD HHMM"){
+createEmployeeRecord(object) 
+new Date(dateStamp);
+const newObj={}
+newObj.type="TimeIn"
+newObj.hour=`${dateStamp.getHours()}${dateStamp.getMinutes()}`
+newObj.date=`${dateStamp.getFullYear()}-${dateStamp.getMonth()}-${dateStamp.getDate()}`
+object.timeInEvents.push(newObj)
+return object
+}
